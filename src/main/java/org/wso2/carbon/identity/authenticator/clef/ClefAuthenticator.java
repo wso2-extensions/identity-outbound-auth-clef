99d7515426486587facf78d56212ae286ea270b5
Rajjaz <hmrajas1990@gmail.com>
2016-03-28 11:39:16 +0530

    Clef authenticator initial commit

---
+++ src/main/java/org/wso2/carbon/identity/authenticator/clef/ClefAuthenticator.java
@@ -0,0 +1,393 @@
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+package org.wso2.carbon.identity.authenticator.clef;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.oltu.oauth2.client.OAuthClient;
+import org.apache.oltu.oauth2.client.URLConnectionClient;
+import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
+import org.apache.oltu.oauth2.client.response.OAuthAuthzResponse;
+import org.apache.oltu.oauth2.client.response.OAuthClientResponse;
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
+import org.apache.oltu.oauth2.common.utils.JSONUtils;
+import org.wso2.carbon.identity.application.authentication.framework.FederatedApplicationAuthenticator;
+import org.wso2.carbon.identity.application.authentication.framework.LocalApplicationAuthenticator;
+import org.wso2.carbon.identity.application.authentication.framework.config.ConfigurationFacade;
+import org.wso2.carbon.identity.application.authentication.framework.config.model.StepConfig;
+import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
+import org.wso2.carbon.identity.application.authentication.framework.exception.AuthenticationFailedException;
+import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
+import org.wso2.carbon.identity.application.authentication.framework.util.FrameworkUtils;
+import org.wso2.carbon.identity.application.authenticator.oidc.OIDCAuthenticatorConstants;
+import org.wso2.carbon.identity.application.authenticator.oidc.OpenIDConnectAuthenticator;
+import org.wso2.carbon.identity.application.common.model.ClaimMapping;
+import org.wso2.carbon.identity.application.common.model.Property;
+import org.wso2.carbon.identity.application.common.util.IdentityApplicationConstants;
+import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
+import org.wso2.carbon.user.api.UserRealm;
+import org.wso2.carbon.user.api.UserStoreException;
+import org.wso2.carbon.user.core.UserStoreManager;
+import org.wso2.carbon.user.core.service.RealmService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Authenticator of clef.
+ */
+public class ClefAuthenticator extends OpenIDConnectAuthenticator implements FederatedApplicationAuthenticator {
+
+    private static Log log = LogFactory.getLog(ClefAuthenticator.class);
+
+    /**
+     * check and process the httpServletRequest to process.
+     *
+     * @param httpServletRequest http request
+     * @return weather true or false
+     */
+    public boolean canHandle(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getParameter(ClefAuthenticatorConstants.CODE) != null;
+    }
+
+    /**
+     * Identify the context.
+     *
+     * @param httpServletRequest http request
+     * @return context identifier
+     */
+    public String getContextIdentifier(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getParameter(ClefAuthenticatorConstants.SESSION_DATA_KEY);
+    }
+
+    /**
+     * Get clef token endpoint.
+     */
+    @Override
+    protected String getTokenEndpoint(Map<String, String> authenticatorProperties) {
+        return ClefAuthenticatorConstants.CLEF_TOKEN_ENDPOINT;
+    }
+
+    /**
+     * Get clef user info endpoint.
+     */
+    @Override
+    protected String getUserInfoEndpoint(OAuthClientResponse token, Map<String, String> authenticatorProperties) {
+        return ClefAuthenticatorConstants.CLEF_USERINFO_ENDPOINT;
+    }
+
+    /**
+     * Check ID token in clef OAuth.
+     */
+    @Override
+    protected boolean requiredIDToken(Map<String, String> authenticatorProperties) {
+        return false;
+    }
+
+    /**
+     * Get the friendly name of the Authenticator
+     */
+    @Override
+    public String getFriendlyName() {
+        return ClefAuthenticatorConstants.AUTHENTICATOR_FRIENDLY_NAME;
+    }
+
+    /**
+     * Get the name of the Authenticator
+     */
+    @Override
+    public String getName() {
+        return ClefAuthenticatorConstants.AUTHENTICATOR_NAME;
+    }
+
+    /**
+     * Get Configuration Properties
+     */
+    @Override
+    public List<Property> getConfigurationProperties() {
+        List<Property> configProperties = new ArrayList<Property>();
+        Property clientId = new Property();
+        clientId.setName(OIDCAuthenticatorConstants.CLIENT_ID);
+        clientId.setDisplayName(ClefAuthenticatorConstants.CLIENT_ID);
+        clientId.setRequired(true);
+        clientId.setDescription("Enter Clef client identifier value");
+        clientId.setDisplayOrder(0);
+        configProperties.add(clientId);
+
+        Property clientSecret = new Property();
+        clientSecret.setName(OIDCAuthenticatorConstants.CLIENT_SECRET);
+        clientSecret.setDisplayName(ClefAuthenticatorConstants.CLIENT_SECRET);
+        clientSecret.setRequired(true);
+        clientSecret.setConfidential(true);
+        clientSecret.setDescription("Enter Clef client secret value");
+        clientSecret.setDisplayOrder(1);
+        configProperties.add(clientSecret);
+
+        Property callbackUrl = new Property();
+        callbackUrl.setDisplayName(ClefAuthenticatorConstants.Callback_URL);
+        callbackUrl.setName(IdentityApplicationConstants.OAuth2.CALLBACK_URL);
+        callbackUrl.setDescription("Enter the callback url");
+        callbackUrl.setDisplayOrder(2);
+        configProperties.add(callbackUrl);
+        return configProperties;
+    }
+
+    /**
+     * This is override because of some values are hard coded and input
+     * values validations are not required.
+     *
+     * @param httpServletRequest    http request
+     * @param httpServletResponse   http response
+     * @param authenticationContext authentication context
+     * @throws AuthenticationFailedException
+     */
+    @Override
+    protected void initiateAuthenticationRequest(HttpServletRequest httpServletRequest,
+                                                 HttpServletResponse httpServletResponse, AuthenticationContext authenticationContext)
+            throws AuthenticationFailedException {
+        AuthenticatedUser authenticatedUser = getUsername(authenticationContext);
+        String username = authenticatedUser.getAuthenticatedSubjectIdentifier();
+        String ClefUserId;
+        String RequestYType;
+        try {
+            UserStoreManager userStoreManager = getUserStoreManager(authenticationContext);
+            ClefUserId = userStoreManager.getUserClaimValue(username,
+                    ClefAuthenticatorConstants.CLEF_ID, null);
+            if (StringUtils.isEmpty(ClefUserId)) {
+                RequestYType = ClefAuthenticatorConstants.LOGIN;
+            } else {
+                RequestYType = ClefAuthenticatorConstants.CONNECT;
+            }
+            String loginPage = ConfigurationFacade.getInstance().getAuthenticationEndpointURL()
+                    .replace(ClefAuthenticatorConstants.LOGIN_PAGE, ClefAuthenticatorConstants.CLEF_PAGE);
+            String queryParams = FrameworkUtils.getQueryStringWithFrameworkContextId(authenticationContext
+                    .getQueryParams(), authenticationContext.getCallerSessionKey(), authenticationContext
+                    .getContextIdentifier());
+            httpServletResponse.sendRedirect(httpServletResponse.encodeRedirectURL(loginPage + ("?"
+                    + queryParams + "&" + OIDCAuthenticatorConstants.CLIENT_ID + "="
+                    + authenticationContext.getAuthenticatorProperties().get(OIDCAuthenticatorConstants.CLIENT_ID))
+                    + "&" + ClefAuthenticatorConstants.REQUEST_TYPE + "=" + RequestYType));
+            authenticationContext.setCurrentAuthenticator(getName());
+        } catch (UserStoreException e) {
+            throw new AuthenticationFailedException(
+                    "Error occured while loading user claim - http://wso2.org/claims/ClefId", e);
+        } catch (IOException e) {
+            throw new AuthenticationFailedException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * This is override because of we are working with claims.
+     *
+     * @param httpServletRequest    http request
+     * @param httpServletResponse   http response
+     * @param authenticationContext authentication context
+     * @throws AuthenticationFailedException
+     */
+    @Override
+    protected void processAuthenticationResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                                 AuthenticationContext authenticationContext)
+            throws AuthenticationFailedException {
+        Map<String, String> authenticatorProperties = authenticationContext.getAuthenticatorProperties();
+        String clientId = authenticatorProperties.get(OIDCAuthenticatorConstants.CLIENT_ID);
+        String clientSecret = authenticatorProperties.get(OIDCAuthenticatorConstants.CLIENT_SECRET);
+        String tokenEndPoint = getTokenEndpoint(authenticatorProperties);
+        String callbackurl = getCallbackUrl(authenticatorProperties);
+        OAuthClient oAuthClient = new OAuthClient(new URLConnectionClient());
+        OAuthClientResponse oAuthResponse;
+        AuthenticatedUser authenticatedUser = getUsername(authenticationContext);
+        String username = authenticatedUser.getAuthenticatedSubjectIdentifier();
+        String ClefUserId;
+        String accessToken;
+        try {
+            OAuthAuthzResponse authzResponse = OAuthAuthzResponse.oauthCodeAuthzResponse(httpServletRequest);
+            String code = authzResponse.getCode();
+            OAuthClientRequest accessRequest = OAuthClientRequest
+                    .tokenLocation(tokenEndPoint)
+                    .setGrantType(GrantType.AUTHORIZATION_CODE)
+                    .setClientId(clientId)
+                    .setClientSecret(clientSecret)
+                    .setRedirectURI(callbackurl)
+                    .setCode(code)
+                    .buildBodyMessage();
+            oAuthResponse = oAuthClient.accessToken(accessRequest);
+            accessToken = oAuthResponse.getParam(ClefAuthenticatorConstants.ACCESS_TOKEN);
+        } catch (OAuthSystemException e) {
+            if (log.isDebugEnabled()) {
+                log.debug("Exception while building request for request access token", e);
+            }
+            throw new AuthenticationFailedException(e.getMessage(), e);
+        } catch (OAuthProblemException e) {
+            if (log.isDebugEnabled()) {
+                log.debug("Exception while requesting code", e);
+            }
+            throw new AuthenticationFailedException(e.getMessage(), e);
+        }
+        if (!StringUtils.isEmpty(accessToken)) {
+            Map<String, Object> userClaims = getUserClaims(oAuthResponse);
+            if (userClaims != null && !userClaims.isEmpty()) {
+                try {
+                    UserStoreManager userStoreManager = getUserStoreManager(authenticationContext);
+                    ClefUserId = userStoreManager.getUserClaimValue(username,
+                            ClefAuthenticatorConstants.CLEF_ID, null);
+                    if (!StringUtils.isEmpty(ClefUserId) && ClefUserId.equals(String
+                            .valueOf(userClaims.get(ClefAuthenticatorConstants.ID)))) {
+                        allowUser(userClaims, authenticationContext);
+                    } else if (StringUtils.isEmpty(ClefUserId)) {
+                        userStoreManager.setUserClaimValue(username, ClefAuthenticatorConstants.CLEF_ID,
+                                String.valueOf(userClaims.get(ClefAuthenticatorConstants.ID)),
+                                ClefAuthenticatorConstants.DEFAULT);
+                        allowUser(userClaims, authenticationContext);
+                    }
+                } catch (UserStoreException e) {
+                    // this exception will be handled by the framework.
+                    throw new AuthenticationFailedException(
+                            "Error occured while loading user claim - ClefId", e);
+                }
+            } else {
+                throw new AuthenticationFailedException("Selected user profile not found");
+            }
+        } else {
+            throw new AuthenticationFailedException("Authentication Failed");
+        }
+    }
+
+    /**
+     * Allow user to login and set claims.
+     *
+     * @param userClaims            user claims
+     * @param authenticationContext authentication context
+     */
+    protected void allowUser(Map<String, Object> userClaims, AuthenticationContext authenticationContext) {
+        AuthenticatedUser authenticatedUserObj;
+        Map<ClaimMapping, String> claims;
+        authenticatedUserObj = AuthenticatedUser
+                .createFederateAuthenticatedUserFromSubjectIdentifier(String
+                        .valueOf(userClaims.get(ClefAuthenticatorConstants.ID)));
+        authenticatedUserObj
+                .setAuthenticatedSubjectIdentifier(String
+                        .valueOf(userClaims.get(ClefAuthenticatorConstants.LAST_NAME)));
+        claims = getSubjectAttributes(userClaims);
+        authenticatedUserObj.setUserAttributes(claims);
+        authenticationContext.setSubject(authenticatedUserObj);
+    }
+
+    /**
+     * Get user information from user info endpoint.
+     *
+     * @param oAuthClientResponse oAuthResponse contain access token
+     * @return user info
+     */
+    protected Map<String, Object> getUserClaims(OAuthClientResponse oAuthClientResponse) {
+        try {
+            String json = sendRequest(
+                    ClefAuthenticatorConstants.CLEF_USERINFO_ENDPOINT,
+                    oAuthClientResponse.getParam(ClefAuthenticatorConstants.ACCESS_TOKEN));
+            Map<String, Object> jsonObject = JSONUtils.parseJSON(json);
+            Map<String, Object> jsonInfoObject = JSONUtils.parseJSON(String
+                    .valueOf(jsonObject.get(ClefAuthenticatorConstants.USER_INFO)));
+            return jsonInfoObject;
+        } catch (IOException e) {
+            log.error(e);
+        }
+        return new HashMap<String, Object>();
+    }
+
+    /**
+     * Get subject attributes.
+     *
+     * @param claimMap Map<String, Object>
+     * @return attributes
+     */
+    protected Map<ClaimMapping, String> getSubjectAttributes(Map<String, Object> claimMap) {
+        Map<ClaimMapping, String> claims = new HashMap<>();
+        if (claimMap != null) {
+            for (Map.Entry<String, Object> entry : claimMap.entrySet()) {
+                claims.put(ClaimMapping.build(entry.getKey(), entry.getKey(), null, false),
+                        entry.getValue().toString());
+                if (log.isDebugEnabled()) {
+                    log.debug("Adding claim from end-point data mapping : "
+                            + entry.getKey() + " <> " + " : " + entry.getValue());
+                }
+            }
+        }
+        return claims;
+    }
+
+    /**
+     * Get user store manager.
+     *
+     * @param authenticationContext authentication context
+     * @return user store manager
+     * @throws AuthenticationFailedException
+     */
+    protected UserStoreManager getUserStoreManager(AuthenticationContext authenticationContext)
+            throws AuthenticationFailedException {
+        AuthenticatedUser authenticatedUser = getUsername(authenticationContext);
+        String tenantDomain;
+        tenantDomain = authenticatedUser.getTenantDomain();
+        int tenantId = IdentityTenantUtil.getTenantId(tenantDomain);
+        RealmService realmService = IdentityTenantUtil.getRealmService();
+        UserRealm userRealm;
+        UserStoreManager userStoreManager;
+        try {
+            userRealm = realmService.getTenantUserRealm(tenantId);
+            userStoreManager = (UserStoreManager) userRealm.getUserStoreManager();
+        } catch (org.wso2.carbon.user.core.UserStoreException e) {
+            // this exception will be handled by the framework.
+            throw new AuthenticationFailedException(
+                    "Error occured while loading user claim - http://wso2.org/claims/ClefId", e);
+        } catch (UserStoreException e) {
+            // this exception will be handled by the framework.
+            throw new AuthenticationFailedException(
+                    "Error occured while loading userrealm/userstoremanager", e);
+        }
+        return userStoreManager;
+    }
+
+    /**
+     * Get username
+     *
+     * @param authenticationContext authentication context
+     * @return username
+     * @throws AuthenticationFailedException
+     */
+    private AuthenticatedUser getUsername(AuthenticationContext authenticationContext) {
+        // username from authentication context.
+        AuthenticatedUser authenticatedUser = null;
+        for (int i = 1; i <= authenticationContext.getSequenceConfig().getStepMap().size(); i++) {
+            StepConfig stepConfig = authenticationContext.getSequenceConfig().getStepMap().get(i);
+            if (stepConfig.getAuthenticatedUser() != null && stepConfig.getAuthenticatedAutenticator()
+                    .getApplicationAuthenticator() instanceof LocalApplicationAuthenticator) {
+                authenticatedUser = stepConfig.getAuthenticatedUser();
+                break;
+            }
+        }
+        return authenticatedUser;
+    }
+}
---
+++ src/main/java/org/wso2/carbon/identity/authenticator/clef/ClefAuthenticatorConstants.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+package org.wso2.carbon.identity.authenticator.clef;
+
+public class ClefAuthenticatorConstants {
+    public static final String AUTHENTICATOR_NAME = "ClefAuthenticator";
+    public static final String AUTHENTICATOR_FRIENDLY_NAME = "clef";
+    public static final String CLEF_OAUTH_ENDPOINT = "https://localhost:9443/ClefEndPoint?";
+    public static final String CLEF_TOKEN_ENDPOINT = "https://clef.io/api/v1/authorize";
+    public static final String CLEF_USERINFO_ENDPOINT = "https://clef.io/api/v1/info";
+    public static final String SESSION_DATA_KEY = "sessionDataKey";
+    public static final String CLIENT_ID = "Client ID";
+    public static final String Callback_URL = "Callback URL";
+    public static final String CLIENT_SECRET = "Client Secret";
+    public static final String CODE = "code";
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String ID = "id";
+    public static final String LAST_NAME = "last_name";
+    public static final String USER_INFO = "info";
+    public static final String CLEF_ID = "http://wso2.org/claims/ClefId";
+    public static final String REQUEST_TYPE = "Request_type";
+    public static final String LOGIN = "login";
+    public static final String CONNECT = "connect";
+    public static final String DEFAULT="Default";
+    public static final String LOGIN_PAGE="login.do";
+    public static final String CLEF_PAGE="ClefEndPoint.jsp";
+}
